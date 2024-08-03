e98fd96677803bed0f3dbf0967b0080ccc688a8e
xhdrkawk1 <55053318+xhdrkawk1@users.noreply.github.com>
2024-08-03 20:32:01 +0900

    Chapter1
    Chapter1 개발 환경 설정

---
+++ Source/HuijunPractice/HuijunPracticeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HuijunPracticeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHuijunPracticeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AHuijunPracticeGameMode();
+};
+
+
+
