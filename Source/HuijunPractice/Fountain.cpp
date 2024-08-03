bda4901c5af1950dbe59a082f808c5224a86a559
xhdrkawk1 <55053318+xhdrkawk1@users.noreply.github.com>
2024-08-03 21:48:49 +0900

    Chapter2
    액터의 설계

--- Source/HuijunPractice/Fountain.cpp
+++ Source/HuijunPractice/Fountain.cpp
@@ -9,6 +9,43 @@ AFountain::AFountain()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BODY"));
+	Water = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WATER"));
+	Light = CreateDefaultSubobject<UPointLightComponent>(TEXT("LIGHT"));
+	Splash = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("SPLASH"));
+
+	RootComponent = Body; // 분수대의 구조물을 담당하기 때문에 Body가 root
+	Water->SetupAttachment(Body); // Water가 body의 자식이 되도록 설정
+	Light->SetupAttachment(Body);
+	Splash->SetupAttachment(Body);
+
+
+	Water->SetRelativeLocation(FVector(0.0f, 0.0f, 135.0f)); // 초기 위치값을 설정
+	Light->SetRelativeLocation(FVector(0.0f, 0.0f, 195.0f));
+	Splash->SetRelativeLocation(FVector(0.0f, 0.0f, 195.0f));
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		SM_BODY(TEXT("/Game/InfinityBladeGrassLands/Environments/Plains/Env_Plains_Ruins/StaticMesh/SM_Plains_Castle_Fountain_01.SM_Plains_Castle_Fountain_01")); // 해당 매쉬를 경로를 저장
+	if (SM_BODY.Succeeded())
+	{
+		Body->SetStaticMesh(SM_BODY.Object);// 해당 매쉬를 Body에 불러옴
+	}
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		SM_WATER(TEXT("/Game/InfinityBladeGrassLands/Effects/FX_Meshes/Env/SM_Plains_Fountain_02.SM_Plains_Fountain_02"));
+
+	if (SM_WATER.Succeeded())
+	{
+		Water->SetStaticMesh(SM_WATER.Object);
+	}
+
+	static ConstructorHelpers::FObjectFinder<UParticleSystem>
+		PS_SPLASH(TEXT("/Game/InfinityBladeGrassLands/Effects/FX_Ambient/Water/P_Water_Fountain_Splash_Base_01.P_Water_Fountain_Splash_Base_01"));
+
+	if (PS_SPLASH.Succeeded())
+	{
+		Splash->SetTemplate(PS_SPLASH.Object);
+	}
 }
 
 // Called when the game starts or when spawned
--- Source/HuijunPractice/Fountain.h
+++ Source/HuijunPractice/Fountain.h
@@ -2,14 +2,14 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "EngineMinimal.h" 
 #include "GameFramework/Actor.h"
 #include "Fountain.generated.h"
 
-UCLASS()
+UCLASS() // 언리얼 객체임을 선언
 class HUIJUNPRACTICE_API AFountain : public AActor
 {
-	GENERATED_BODY()
+	GENERATED_BODY() // 언리얼 객체임을 선언 
 	
 public:	
 	// Sets default values for this actor's properties
@@ -22,5 +22,21 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnyWhere) // 언리얼 엔진에서 자동으로 객체 관리 언리얼 오브젝트만 가능, VisibleAnyWhere을 디테일 윈도우에서 속성 값을 편집 할 수 있음
+		UStaticMeshComponent* Body;
+
+	UPROPERTY(VisibleAnyWhere)
+		UStaticMeshComponent* Water;
+
+	UPROPERTY(VisibleAnyWhere)
+		UPointLightComponent* Light;
+
+	UPROPERTY(VisibleAnyWhere)
+		UParticleSystemComponent* Splash;
+
+	UPROPERTY(EditAnyWhere, Category=ID) // 언리얼 에디터에서 속성의 데이터를 변경하려면 VisibleAnywhere가 아닌 EditAnyWhere을 사용해야함 
+		//Category = 분류명 규칙으로키워드를 추가하면, 지정한 분류에서 속성값을 관리
+		int32 ID;
 
 };
